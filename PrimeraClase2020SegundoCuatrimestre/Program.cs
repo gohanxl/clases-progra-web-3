2f39bc547a9bcc7b7dd5aa4d5f8aeaeaf13e9d9d
gohanxl <lucasmlacquaniti@gmail.com>
2020-09-02 20:19:39 -0300

    First commit

--- PrimeraClase2020SegundoCuatrimestre/Program.cs
+++ PrimeraClase2020SegundoCuatrimestre/Program.cs
@@ -13,11 +13,13 @@ namespace PrimeraClase2020SegundoCuatrimestre
             Console.WriteLine("---Sumando umeros---");
             Console.Write("Ingrese un numero");
             string input1 = Console.ReadLine();
+            int num1 = Convert.ToInt32(input1);
 
             Console.Write("Ingrese otro numero numero");
             string input2 = Console.ReadLine();
+            int num2 = Convert.ToInt32(input2);
 
-            Console.Write("La suma es: " + num1 + num2);
+            Console.Write("La suma es: " + (num1 + num2));
             Console.ReadKey();
         }
     }
