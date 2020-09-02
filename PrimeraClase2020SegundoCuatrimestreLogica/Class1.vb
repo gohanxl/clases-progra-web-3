773d963c907667b4436696d6a5f933ade1f24192
gohanxl <lucasmlacquaniti@gmail.com>
2020-09-02 20:35:56 -0300

    Create Class Library

---
+++ PrimeraClase2020SegundoCuatrimestreLogica/Class1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Calculadora
+
+    {
+        Public Static int Sumar(String input1, String input2)
+        {
+            int num1 = Convert.ToInt32(input1);
+            int num2 = Convert.ToInt32(input2);
+
+            Return num1 + num2;
+        }
+    }
+End Class
