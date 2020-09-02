8f79069dc4c42a5f72894b2cf7cad7beeefb9f02
gohanxl <lucasmlacquaniti@gmail.com>
2020-09-02 20:10:20 -0300

    Add project files.

---
+++ PrimeraClase2020SegundoCuatrimestre/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraClase2020SegundoCuatrimestre
+{
+    class Calculadora
+    {
+        public int Sumar(string input1, string input2)
+        {
+            int num1 = Convert.ToInt32(input1);
+            int num2 = Convert.ToInt32(input2);
+
+            return num1 + num2;
+        }
+    }
+}
---
+++ PrimeraClase2020SegundoCuatrimestre/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraClase2020SegundoCuatrimestre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Sumando umeros---");
+            Console.Write("Ingrese un numero");
+            string input1 = Console.ReadLine();
+
+            Console.Write("Ingrese otro numero numero");
+            string input2 = Console.ReadLine();
+
+            Console.Write("La suma es: " + num1 + num2);
+            Console.ReadKey();
+        }
+    }
+}
